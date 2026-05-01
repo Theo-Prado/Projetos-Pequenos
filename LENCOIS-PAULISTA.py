2449544c96c6a2e03a27e9476717b97b04a14b16
Theo <theo.silva.prado@gmail.com>
2026-05-01 15:50:14 -0300

    Add celebration message for Lençóis Paulista's anniversary

---
+++ LENCOIS-PAULISTA.py
@@ -0,0 +1,14 @@
+idade = 167
+vinte_e_oito_de_outubro = True
+if vinte_e_oito_de_outubro:
+  idade = idade + 1
+  print(f"Parabéns Lençóis Paulista! {idade} anos de história!")
+  print("Código Kids, formando o futuro da próxima geração de lençoenses!")
+print(r"___________________________")
+print(r"|  /\/\/\   /\/\/\   /\/\ |")
+print(r"| /      \ /      \ /     |")
+print(r"|{  ~  ~  ~  ~  ~  ~  ~  }|")
+print(r"|{  PRO DEO, PRO PATRIA  }|")
+print(r"|{  ~  ~  ~  ~  ~  ~  ~  }|")
+print(r"| \      / \      / \     |")
+print(r"|__\/\/\/___\/\/\/___\/\/_|")
